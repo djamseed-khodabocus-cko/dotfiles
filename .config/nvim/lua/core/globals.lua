f32281d661277da038a08d2674624e57b299ece1
Djamseed Khodabocus <djamseed.khodabocus@checkout.com>
2025-01-06 14:05:29 +0400

    nvim setup rewrite

--- .config/nvim/lua/core/globals.lua
+++ .config/nvim/lua/core/globals.lua
@@ -1,4 +1,3 @@
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
--- Github Copilot
-vim.g.copilot_no_tab_map = true
+vim.g.mapleader = ' '
+vim.g.maplocalleader = '\\'
+vim.g.have_nerd_font = true
