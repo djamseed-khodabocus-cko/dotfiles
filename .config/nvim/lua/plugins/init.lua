1da8891e1df0b506b1241de29d045d0bf9d06955
Djamseed Khodabocus <djamseed.khodabocus@checkout.com>
2025-08-29 17:21:36 +0400

    refactor(nvim): regroup mini plugins

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -3,23 +3,12 @@
 return {
   -- resuable Lua functions
   { 'nvim-lua/plenary.nvim', lazy = true },
-  -- icon provider (glyph or ascii)
-  { 'echasnovski/mini.icons', version = '*', opts = {}, lazy = true },
   -- automatic indentation style detection
   { 'nmac427/guess-indent.nvim', event = { 'BufReadPost', 'BufNewFile' } },
-  -- add character pairs automatically
-  { 'echasnovski/mini.pairs', version = '*', event = { 'InsertEnter' }, opts = {} },
   -- improve viewing markdown files in Neovim
   { 'MeanderingProgrammer/render-markdown.nvim', ft = 'markdown', opts = {} },
   -- Neovim notifications and LSP progress messages
   { 'j-hui/fidget.nvim', event = 'LspAttach', opts = { notification = { window = { winblend = 0 } } } },
-  -- add, delete, replace, find, highlight surrounding (brakets, quotes, etc)
-  --     add surrounding with sa (in visual mode or on motion).
-  --     delete surrounding with sd.
-  --     replace surrounding with sr.
-  --     find surrounding with sf or sF (move cursor right or left).
-  --     highlight surrounding with sh.
-  { 'echasnovski/mini.surround', version = '*', event = { 'BufReadPost', 'BufNewFile' }, opts = {} },
   -- Smart and Powerful commenting
   -- NORMAL mode
   --     gcc -> Linewise comment
