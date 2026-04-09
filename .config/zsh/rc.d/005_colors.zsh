d3376446ebe9e566f2411f80d07958dbad505829
Djamseed Khodabocus <djamseed.khodabocus@checkout.com>
2026-04-09 12:00:39 +0400

    feat: generate ANSI theme with vivid (for LS_COLORS)

--- .config/zsh/rc.d/005_colors.zsh
+++ .config/zsh/rc.d/005_colors.zsh
@@ -3,4 +3,4 @@ export CLICOLOR=1
 export LSCOLORS=GxFxCxDxCxegedabagaced
 
 # Use for `eza` output
-(($+commands[vivid])) && export LS_COLORS=$(vivid generate rose-pine-moon)
+(($+commands[vivid])) && export LS_COLORS=$(vivid generate ansi)
